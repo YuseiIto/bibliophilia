89d512eba9fd3dcd79dbd992885f4ce6ebc39096
Yusei Ito <me@yuseiito.com>
2024-12-02 14:43:47 +0900

    fix: `/new` をRadixからshadcn/uiに移行

--- app/components/manual_register_form.tsx
+++ app/components/manual_register_form.tsx
@@ -1,10 +1,9 @@
 import { Form } from "@remix-run/react";
-import { Text } from "@radix-ui/themes";
 
 export function ManualRegisterForm() {
   return (
     <Form method="post" action="/register_manually">
-      <Text> 手動登録 </Text>
+      <p> 手動登録 </p>
     </Form>
   );
 }
--- app/routes/new.tsx
+++ app/routes/new.tsx
@@ -2,6 +2,13 @@ import type { MetaFunction } from "@remix-run/cloudflare";
 import { Form } from "@remix-run/react";
 import { ManualRegisterForm } from "~/components/manual_register_form";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { Input } from "~/components/ui/input";
+import { Table, TableBody, TableCell, TableHeader, TableHead, TableRow } from "~/components/ui/table";
+import { Button } from "~/components/ui/button";
+import { Checkbox } from "~/components/ui/checkbox";
+import { FilePlus } from "@mynaui/icons-react";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Bibliophilia" },
@@ -11,67 +18,64 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <p>Hello</p>
-    /*
-    <Flex m="5" direction="column">
-      <Heading size="8"> Register </Heading>
-      <Section>
-        <Tabs.Root defaultValue="isbn">
-          <Tabs.List>
-            <Tabs.Trigger value="isbn">ISBN</Tabs.Trigger>
-            <Tabs.Trigger value="manually">手入力</Tabs.Trigger>
-          </Tabs.List>
+    <div className="flex m-5 flex-col">
+      <h1> Register </h1>
+      <section className="m-3">
+        <Tabs defaultValue="isbn">
+          <TabsList>
+            <TabsTrigger value="isbn">ISBN</TabsTrigger>
+            <TabsTrigger value="manually">手入力</TabsTrigger>
+          </TabsList>
 
-          <Box pt="3">
-            <Tabs.Content value="isbn">
-              <Form method="post" action="/register_by_isbn">
-                <Flex direction="row">
-                  <Box pr="2" width="80%">
-                    <TextField.Root placeholder="ISBN-10またはISBN-13 例: 4003107012" name="isbn" >
-                    </TextField.Root>
-                  </Box>
-                  <Box width="20%">
-                    <Button type="submit" variant="surface">検索</Button>
-                  </Box>
-                </Flex>
-              </Form>
-              <Section>
-                <Table.Root>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.ColumnHeaderCell width="1em"><Checkbox></Checkbox></Table.ColumnHeaderCell>
-                      <Table.ColumnHeaderCell width="13em">ISBN</Table.ColumnHeaderCell>
-                      <Table.ColumnHeaderCell>タイトル</Table.ColumnHeaderCell>
-                      <Table.ColumnHeaderCell>著編者</Table.ColumnHeaderCell>
-                      <Table.ColumnHeaderCell>出版年月</Table.ColumnHeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    <Table.Row>
-                      <Table.Cell max-width="1">
-                        <Checkbox />
-                      </Table.Cell>
-                      <Table.Cell >978-4-87311-904-5</Table.Cell>
-                      <Table.Cell>プログラミングTypeScript</Table.Cell>
-                      <Table.Cell>オライリージャパン</Table.Cell>
-                      <Table.Cell>2020</Table.Cell>
-                    </Table.Row>
-                  </Table.Body>
-                </Table.Root>
-              </Section>
-              <Flex direction="row" justify="center">
-                <Button size="3" variant="soft"> <FilePlusIcon /> 選択中の資料を登録</Button>
-              </Flex>
-            </Tabs.Content>
+          <TabsContent value="isbn">
+            <Form method="post" action="/register_by_isbn">
+              <div className="flex flex-row">
+                <div className="pr-2 width-[80%]">
+                  <Input placeholder="ISBN-10またはISBN-13 例: 4003107012" name="isbn" >
+                  </Input>
+                </div>
+                <div className="width=[20%]">
+                </div>
+              </div>
+            </Form>
 
-            <Tabs.Content value="manually">
-              <ManualRegisterForm />
-            </Tabs.Content>
-          </Box>
-        </Tabs.Root>
-      </Section>
-    </Flex>
-     */
+            <section>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead ><Checkbox></Checkbox></TableHead>
+                    <TableHead >ISBN</TableHead>
+                    <TableHead >タイトル</TableHead>
+                    <TableHead >著編者</TableHead>
+                    <TableHead>出版年月</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell max-width="1">
+                      <Checkbox />
+                    </TableCell>
+                    <TableCell >978-4-87311-904-5</TableCell>
+                    <TableCell>プログラミングTypeScript</TableCell>
+                    <TableCell>オライリージャパン</TableCell>
+                    <TableCell>2020</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </section>
+
+            <div className="flex flex-row justify-center">
+              <Button> <FilePlus /> 選択中の資料を登録</Button>
+            </div >
+
+          </TabsContent>
+
+          <TabsContent value="manually">
+            <ManualRegisterForm />
+          </TabsContent>
+        </Tabs>
+      </section>
+    </div>
   );
 }
 
