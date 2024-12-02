7d7e01a0d7462a8f6da16b34ed09f1037fa1fc24
Yusei Ito <me@yuseiito.com>
2024-12-02 22:22:31 +0900

    feat: Logoの文字列を描くコンポーネント`LogoType` を実装

---
+++ app/components/logo-type.tsx
@@ -0,0 +1,14 @@
+import type { CSSProperties } from "react";
+
+export function LogoType() {
+
+  const logoTypeStyle: CSSProperties = {
+    fontFamily: "Futura, ui-sans-serif,\"-apple-system\", Helvetica, Jost, sans-serif",
+    fontOpticalSizing: "auto",
+    fontWeight: 500,
+    fontStyle: "normal"
+  };
+
+  return (<span className="logo-type text-xl ml-3 font-[]" style={logoTypeStyle}
+  > Bibliophilia </span>);
+}
