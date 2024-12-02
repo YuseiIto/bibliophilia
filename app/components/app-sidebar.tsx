2fbe5a1daeb9f19f9245518d41654bc951bb6ab8
Yusei Ito <me@yuseiito.com>
2024-12-02 16:48:40 +0900

    feat: Default Layoutをいい感じにする(wip)

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,20 +1,46 @@
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarRail
 } from "~/components/ui/sidebar"
+
+import { Search } from "@mynaui/icons-react"
+import LogoIcon from "~/assets/icon.png";
+
 
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarHeader />
+    <Sidebar collapsible="icon">
+      <SidebarHeader >
+        <div className="flex flex-row gap-2 items-center">
+          <img src={LogoIcon} className="max-w-10"/>
+          <span className="text-xl"> Bibliophilia </span>
+        </div>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup >
+          <SidebarGroupLabel> Search </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton tooltip="Find items">
+                  <Search />
+                  <span>Search</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarRail />
     </Sidebar>
   )
 }
--- app/layouts/default.tsx
+++ app/layouts/default.tsx
@@ -1,14 +1,22 @@
 import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 import { AppSidebar } from "~/components/app-sidebar";
+import { SidebarInset } from "~/components/ui/sidebar";
+import { Separator } from "~/components/ui/separator";
 
-export function DefaultLayout ({children}: {children: React.ReactNode}){
+export function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
+
     <SidebarProvider>
-      <AppSidebar/>
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-8 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+        </header>
+        <main>
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider >
   )
 }
