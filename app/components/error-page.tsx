a87be1187538b3cff70ae04be6b197436a04286f
Yusei Ito <me@yuseiito.com>
2024-12-03 21:07:38 +0900

    fix: RouteError以外にも対応できるようにする(wip)

--- app/components/error-page.tsx
+++ app/components/error-page.tsx
@@ -1,15 +1,23 @@
-import { NavLink } from "@remix-run/react";
+import { NavLink, isRouteErrorResponse } from "@remix-run/react";
 import logo from "~/assets/logo.png";
 
-export function ErrorPage({ error }) {
+function RouteError({ error }) {
 
-  console.log(error)
-  return (<main className="flex flex-col p-8 h-screen justify-center">
+  return (
     <div className="flex flex-row gap-8 items-center justify-center">
       <div><h1 className="font-bold text-9xl">{error.status}</h1></div>
       <div><p className="font-bold text-6xl">{error.statusText}</p></div>
     </div>
-    <div className="flex flex-row w-full justify-center">
+  )
+}
+export function ErrorPage({ error }) {
+
+  console.log(error)
+  return (<main className="flex flex-col p-8 h-screen justify-center">
+    <div className="flex flex-col w-full items-center">
+      {
+        isRouteErrorResponse(error) ? <RouteError error={error} /> : <p> {error} </p>
+      }
       <div className="max-w-[300px]">
         <NavLink to="/">
           <img src={logo} alt="Bibliophilia" />
