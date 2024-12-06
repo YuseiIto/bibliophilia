51f52b55e922150641b100dc467b44e859491d6b
Yusei Ito <me@yuseiito.com>
2024-12-06 17:43:43 +0900

    chore: abstractのテストを追加

--- app/model/ndl-search/dcndl/dcndl.test.ts
+++ app/model/ndl-search/dcndl/dcndl.test.ts
@@ -152,6 +152,10 @@ describe("エルマーのぼうけん(test-fixtures/my-fathers-dragon.rdf)", asy
 		expect(parser.dcndlPartInformation).toMatchObject([]);
 	});
 
+	test("dcterms:abstract", () => {
+		expect(parser.dctermsAbstract).toMatchObject(["どうぶつ島をたずねた少年の物語。ユーモアとナンセンスと現実味を融合させて描いた米国で評判の童話。(日本図書館協会)"]);
+	});
+
 	test("dcterms:subject", () => {
 		expect(parser.dctermsSubject).toMatchObject([]);
 	});
