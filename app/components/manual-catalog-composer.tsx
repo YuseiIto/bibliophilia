86521d7274ef5bfbe86242d6f78ab03bf95bc136
Yusei Ito <me@yuseiito.com>
2024-12-07 14:06:20 +0900

    feat: 目録規則の入力をcomboboxでできるようにする

--- app/components/manual-catalog-composer.tsx
+++ app/components/manual-catalog-composer.tsx
@@ -1,64 +1,70 @@
 import { Form } from "@remix-run/react";
 import { Input } from "./ui/input";
 
+import { Combobox } from "~/components/combobox";
+import { PrefferedTitleInput } from "~/components/preffered_title_input";
+
 export function ManualCatalogComposer() {
+	const catalogingRules = [
+		{
+			label: "日本目録規則2018年版 (NCR2018)",
+			value: "ncr/2018",
+		},
+		{
+			label: "日本目録規則1987年版 (NCR1987)",
+			value: "ncr/1987",
+		},
+		{
+			label: "日本目録規則1977年版 (NCR1977)",
+			value: "ncr/1977",
+		},
+		{
+			label: "日本目録規則 (NCR)",
+			value: "ncr",
+		},
+		{
+			label: "国立国会図書館逐次刊行物目録規則",
+			value: "ndlserial",
+		},
+		{
+			label: "RDA (Resource Description and Access)",
+			value: "rda",
+		},
+		{
+			label: "AACR (Anglo-American Cataloging Rules)",
+			value: "aacr",
+		},
+	];
+
 	return (
 		<Form method="post" action="/register_manually">
 			<div className="flex flex-col gap-3">
-			<Input
-				placeholder="主に使用するタイトル"
-				name="preferred_title"
-			></Input>
-
-			<Input
-				placeholder="主に使用するタイトルの読み"
-				name="prefered_title_transcription"
-			></Input>
-
-
-				/* Selectにする */
-			<Input
-				placeholder="目録情報の出典"
-				name="catalog_source"
-			></Input>
-
-				/* Selectにする */
-			<Input
-				placeholder="目録情報の出典区分"
-				name="catalog_source_type"
-			></Input>
-
-				/* Selectにする */
-			<Input
-				placeholder="目録情報の出典区分"
-				name="cataloging_rule"
-			></Input>
-
-
-			<Input
-				placeholder="サムネイルURL"
-				name="thumbnail_url"
-			></Input>
-
-			<Input
-				placeholder="巻号"
-				name="preferred_volume"
-			></Input>
-
-			<Input
-				placeholder="巻号タイトル"
-				name="preferred_volume_title"
-			></Input>
-
-			<Input
-				placeholder="巻号タイトルよみ"
-				name="preferred_volume_title_transcription"
-			></Input>
-			
+				<PrefferedTitleInput />
 				<Input
-				placeholder="ISBN-10またはISBN-13 例: 4003107012"
-				name="isbn"
-			></Input>
+					placeholder="主に使用するタイトルの読み"
+					name="prefered_title_transcription"
+				></Input>
+				<Combobox options={catalogingRules} label="目録規則を選択"/>
+
+				<Input
+					placeholder="目録情報の出典区分"
+					name="catalog_source_type"
+				></Input>
+				<Input placeholder="目録情報の出典（URL等）" name="catalog_source"></Input>
+
+				/* Selectにする */
+				<Input placeholder="目録情報の出典区分" name="cataloging_rule"></Input>
+				<Input placeholder="サムネイルURL" name="thumbnail_url"></Input>
+				<Input placeholder="巻号" name="preferred_volume"></Input>
+				<Input placeholder="巻号タイトル" name="preferred_volume_title"></Input>
+				<Input
+					placeholder="巻号タイトルよみ"
+					name="preferred_volume_title_transcription"
+				></Input>
+				<Input
+					placeholder="ISBN-10またはISBN-13 例: 4003107012"
+					name="isbn"
+				></Input>
 			</div>
 		</Form>
 	);
---
+++ app/components/preffered_title_input.tsx
@@ -0,0 +1,11 @@
+import { Input } from "~/components/ui/input"
+
+export function PrefferedTitleInput() {
+	return (
+		<Input
+			placeholder="優先タイトル"
+			name="preferred_title"
+		></Input>
+	);
+
+}
