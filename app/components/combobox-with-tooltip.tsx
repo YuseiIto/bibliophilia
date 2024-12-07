91e2d1ef77c0642b51c0ac89185e2df37e04d8dd
Yusei Ito <me@yuseiito.com>
2024-12-07 16:20:23 +0900

    feat: `ComboboxWithToolTip` を作成

---
+++ app/components/combobox-with-tooltip.tsx
@@ -0,0 +1,42 @@
+import { Combobox, ComboboxOption } from "~/components/combobox";
+import {
+	TooltipProvider,
+	Tooltip,
+	TooltipTrigger,
+	TooltipContent,
+} from "~/components/ui/tooltip";
+import { InfoCircle } from "@mynaui/icons-react";
+
+interface ComboboxWithTooltipProps {
+	name?: string;
+	label: string;
+	options: ComboboxOption[];
+	children?: ReactNode;
+}
+
+export function ComboboxWithTooltip({
+	name,
+	label,
+	options,
+	children,
+}: ComboboxWithTooltipProps) {
+	return (
+		<TooltipProvider>
+			<div className="flex flex-row gap-2 items-center">
+				<Combobox
+					className="w-full"
+					label={label}
+					name={name}
+					options={options}
+				/>
+
+				<Tooltip>
+					<TooltipTrigger tabIndex={-1}>
+						<InfoCircle size={18} />
+					</TooltipTrigger>
+					<TooltipContent>{children}</TooltipContent>
+				</Tooltip>
+			</div>
+		</TooltipProvider>
+	);
+}
