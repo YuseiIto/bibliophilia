aa40ecc899cc18a10e191ba719fe3e4014878e2c
Yusei Ito <me@yuseiito.com>
2024-12-08 21:45:12 +0900

    feat: 識別子ダイアログを閉じられるようにする

--- app/components/identifier-input.tsx
+++ app/components/identifier-input.tsx
@@ -1,4 +1,4 @@
-import { Card } from "~/components/ui/card";
+import { useState } from "react";
 import { Input } from "~/components/ui/input";
 import { Combobox } from "~/components/combobox";
 import { Button } from "~/components/ui/button";
@@ -11,15 +11,23 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "~/components/ui/dialog";
-import { InfoCircle } from "@mynaui/icons-react";
 
-export function IdentifierDialog() {
+interface IdentifierDialogProps {
+	onOpenChange: (isOpen: boolean) => void;
+}
+
+export function IdentifierDialog({ onOpenChange }: IdentifierDialogProps) {
 	const identifierTypeOptions = [
 		{
 			label: "JP番号(日本全国書誌番号)",
 			value: "http://ndl.go.jp/dcndl/terms/JPNO",
 		},
 	];
+
+	const onSave = (event: React.FormEvent) => {
+		event.preventDefault();
+		onOpenChange(false);
+	};
 
 	return (
 		<DialogContent className="flex flex-col p-3">
@@ -29,27 +37,28 @@ export function IdentifierDialog() {
 					ISBNや各種MARC番号など、資料の識別に使用される符号を指定します。
 				</DialogDescription>
 			</DialogHeader>
-			<Combobox label="識別子の種類を選択" options={identifierTypeOptions} />
-			<div className="flex flex-row">
-				<Input />
-			</div>
+				<Combobox label="識別子の種類を選択" options={identifierTypeOptions} />
+				<div className="flex flex-row">
+					<Input />
+				</div>
 
-			<Button>
-				<Plus /> 識別子を追加
-			</Button>
+				<Button type="button" onClick={onSave}>
+					<Plus /> 識別子を追加
+				</Button>
 		</DialogContent>
 	);
 }
 
 export function IdentifierInput() {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<Dialog>
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
 			<DialogTrigger asChild>
 				<Button size="sm" variant="ghost">
 					<Plus /> 識別子を追加
 				</Button>
 			</DialogTrigger>
-			<IdentifierDialog />
+			<IdentifierDialog onOpenChange={setIsOpen} />
 		</Dialog>
 	);
 }
