6ea5377d329206d33962b334afa1dc8d2f3642f4
Yusei Ito <me@yuseiito.com>
2024-12-12 13:26:17 +0900

    feat: NDC9, NDC10, NDLCをsubjectに追加

--- app/model/subject.ts
+++ app/model/subject.ts
@@ -1,13 +1,15 @@
-
 export const subjectTypes = {
+	"http://id.ndl.go.jp/class/ndlc": "国立国会図書館表 (NDLC)",
 	"http://ndl.go.jp/dcndl/terms/NDC8": "日本十進分類法第8版 (NDC8)",
+	"http://id.ndl.go.jp/class/ndc9": "日本十進分類法第9版 (NDC9)",
+	"http://id.ndl.go.jp/class/ndc10": "日本十進分類法第10版 (NDC10)",
 	"http://ndl.go.jp/dcndl/terms/NDC": "日本十進分類法 (NDC)",
 	"http://ndl.go.jp/dcndl/terms/LCC": "米国国会図書館分類法 (LCC)",
 	"http://ndl.go.jp/dcndl/terms/UDC": "国際十進分類法 (UDC)",
 	"http://ndl.go.jp/dcndl/terms/GHQSCAP": "GHQ/SCAP資料分類",
 	"http://ndl.go.jp/dcndl/terms/USCAR": "USCAR資料分類",
 	"http://ndl.go.jp/dcndl/terms/MCJ": "米海兵隊太平洋戦争(MCJ)文書分類番号",
-	"http://purl.org/dc/terms/DDC": "ディーイ十進分類法",
+	"http://dewey.info/class/": "デューイ十進分類法",
 	other: "その他",
 };
 
