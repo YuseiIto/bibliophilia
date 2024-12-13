8d6aa76ab25a6f649ec27bc796499dcf632e15ca
Yusei Ito <me@yuseiito.com>
2024-12-13 19:47:05 +0900

    feat: ISBNからNDLのサムネイルを探して表示するようにする

--- app/components/bib-thumbnail-item.tsx
+++ app/components/bib-thumbnail-item.tsx
@@ -5,10 +5,27 @@ interface BibThumbnailItemProps {
 }
 
 export function BibThumbnailItem({ value }: BibThumbnailItemProps) {
-	console.log(value);
+	console.log(value.identifiers);
+
+	const isbn = value.identifiers
+		.filter(
+			({ identifier_type }) =>
+				identifier_type === "http://ndl.go.jp/dcndl/terms/ISBN",
+		)
+		.map(({ identifier }) => identifier)[0]
+		.replaceAll("-", "");
+
+	const ndlThumbnail = isbn
+		? `https://ndlsearch.ndl.go.jp/thumbnail/${isbn}.jpg`
+		: null;
+
 	return (
 		<div className="flex flex-col items-center gap-1">
-			<Skeleton className="w-[130px] h-[200px]" />
+			{ndlThumbnail ? (
+				<img src={ndlThumbnail} />
+			) : (
+				<Skeleton className="h-[200px] w-[130px]" />
+			)}
 			<span className="font-bold text-xs"> {value.preferred_title} </span>
 		</div>
 	);
