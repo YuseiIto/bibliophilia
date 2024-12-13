59ed06186929e5ebbdb69de06eb16b4fcad310e8
Yusei Ito <me@yuseiito.com>
2024-12-13 18:59:06 +0900

    feat: サムネイル下にタイトルを表示するようにする

--- app/components/bib-thumbnail-item.tsx
+++ app/components/bib-thumbnail-item.tsx
@@ -6,5 +6,10 @@ interface BibThumbnailItemProps {
 
 export function BibThumbnailItem({ value }: BibThumbnailItemProps) {
 	console.log(value);
-	return <Skeleton className="w-[130px] h-[200px]" />;
+	return (
+		<div className="flex flex-col items-center gap-1">
+			<Skeleton className="w-[130px] h-[200px]" />
+			<span className="font-bold text-xs"> {value.preferred_title} </span>
+		</div>
+	);
 }
