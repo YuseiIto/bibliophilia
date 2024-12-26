b228715993e856d74865dc550b3075a641296707
Yusei Ito <me@yuseiito.com>
2024-12-26 19:03:45 +0900

    fix: `Env`の名前解決に関わるlint errorsを修正

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
+import type { Env } from "~/cloudflare";
 import {
 	bibWorksTable,
 	bibIdentifiersTable,
--- load-context.ts
+++ load-context.ts
@@ -1,11 +1,10 @@
 import { type PlatformProxy } from "wrangler";
+import type { Env } from "~/cloudflare";
 
 // When using `wrangler.toml` to configure bindings,
 // `wrangler types` will generate types for those bindings
 // into the global `Env` interface.
-// Need this empty interface so that typechecking passes
 // even if no `wrangler.toml` exists.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
