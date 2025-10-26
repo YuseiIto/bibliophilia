bf4c8dde922363928cc7090843c2c3e1d7087397
Yusei Ito <me@yuseiito.com>
2025-10-26 13:22:27 +0900

    fix: Migrate `json` to `data`

--- app/routes/register_by_isbn.tsx
+++ app/routes/register_by_isbn.tsx
@@ -1,11 +1,11 @@
-import { ActionFunction, redirect, json } from "react-router";
+import { ActionFunction, redirect, data } from "react-router";
 
 export const action: ActionFunction = async ({ request }) => {
 	const formData = await request.formData();
 	const isbn = formData.get("isbn");
 
 	if (typeof isbn !== "string" || isbn.length === 0) {
-		return json({ errors: { title: "ISBN is required" } }, { status: 422 });
+		return data({ errors: { title: "ISBN is required" } }, { status: 422 });
 	}
 
 	console.log(`ISBN: ${isbn}`);
