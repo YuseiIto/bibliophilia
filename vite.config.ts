4a1be71b417bc917fb7cce6ee318bfec85f1f58b
Yusei Ito <me@yuseiito.com>
2025-10-26 12:52:56 +0900

    fix: Add react-router config

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,5 @@ declare module "@remix-run/cloudflare" {
 }
 
 export default defineConfig({
-	plugins: [
-		cloudflareDevProxy(),
-		reactRouter({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-				v3_singleFetch: true,
-				v3_lazyRouteDiscovery: true,
-			},
-		}),
-		tsconfigPaths(),
-	],
+	plugins: [cloudflareDevProxy(), reactRouter(), tsconfigPaths()],
 });
