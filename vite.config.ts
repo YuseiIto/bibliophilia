90a857c7f6b22ac2d8cbb4bf60e517f569b0b31c
Yusei Ito <me@yuseiito.com>
2025-10-26 13:24:49 +0900

    fix: remove unused future flags decleration for remix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/cloudflare" {
-	interface Future {
-		v3_singleFetch: true;
-	}
-}
-
 export default defineConfig({
 	plugins: [cloudflareDevProxy(), reactRouter(), tsconfigPaths()],
 });
