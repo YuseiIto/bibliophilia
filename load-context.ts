b4326160aa6eaa5751f226aad8d8d1d84cf95677
Yusei Ito <me@yuseiito.com>
2025-10-26 13:30:12 +0900

    fix: Cleanup AppLoadContext

--- load-context.ts
+++ load-context.ts
@@ -12,16 +12,6 @@ declare module "react-router" {
 	interface AppLoadContext {
 		cloudflare: Cloudflare;
 	}
-
-	  // TODO: remove this once we've migrated to `Route.LoaderArgs` instead for our loaders
-  interface LoaderFunctionArgs {
-    context: AppLoadContext;
-  }
-
-  // TODO: remove this once we've migrated to `Route.ActionArgs` instead for our actions
-  interface ActionFunctionArgs {
-    context: AppLoadContext;
-  }
 }
 
 export {}; // necessary for TS to treat this as a module
